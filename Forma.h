9524cd6732f84519d7e3fc1d443904f78e103b9a
Cris22110093 <113583925+Cris22110093@users.noreply.github.com>
2022-09-15 02:30:19 -0500

    Create Forma.h

---
+++ Forma.h
@@ -0,0 +1,14 @@
+#pragma once
+class PolvoCosmico {
+public:
+	virtual void setcoorY(int) {}
+	virtual void setcoorX(int) {}
+	virtual void settam(int) {}
+	virtual void setdibujar() {}
+	virtual void settipo(int) {}
+	/*virtual int getcoorX() { return; }
+	virtual int getcoorY() { return; }
+	virtual int gettam() { return; }
+	virtual int gettipo() { return; }
+	*/
+};
